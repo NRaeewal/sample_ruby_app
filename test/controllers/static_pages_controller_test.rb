d678e56aba68502dd3670611eaf198c219c1b400
Nar Raeewal <nraeewal@gmail.com>
2022-05-30 09:14:17 -0600

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,11 +8,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
 
   test "should get home" do
+    get root_path
     get static_pages_home_url
     assert_response :success
     assert_select "title", "Ruby on Rails Tutorial Sample App"
 
   test "should get help" do
+    get help_path
     get static_pages_help_url
     assert_response :success
     assert_select "title", "Ruby on Rails Tutorial Sample App"
@@ -22,9 +24,17 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
 
   test "should get about page" do
+    get about_path
     get static_pages_about_url
     assert_response :success
     assert_select "title", "Ruby on Rails Tutorial Sample App"
-
   end 
+
+  test "should get contact" do
+    get contact_path
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+  end
+
 end
